f64f047f95080c253d6bdc24ec9a87c9fcf40ffe
LeeKangJun33 <drfting1230@naver.com>
2023-05-13 19:05:30 +0900

    Init: Project 생성

---
+++ PT_Service/src/test/java/com/example/pt_service/PtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
